362d92723e943f1d189eb3098072150fc88e4608
Emiliano Montesdeoca <emiliano.montesdeoca@intelequia.com>
2022-12-09 15:42:02 +0000

    feat: project

---
+++ DotNet6CustomAttribute/Attributes/HeaderCheckAttribute.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace DotNet6CustomAttribute.Attributes
+{
+    public class HeaderCheckAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Get all headers
+            var headers = context.HttpContext.Request.Headers;
+
+            // Check if headers has x-dotnet-6-custom-attribute
+            if (!headers.ContainsKey("x-dotnet-6-custom-attribute"))
+            {
+                context.Result = new BadRequestObjectResult("The header x-dotnet-6-custom-attribute is missing");
+            }
+            else if (string.IsNullOrEmpty(headers["x-dotnet-6-custom-attribute"]))
+            {
+                context.Result = new BadRequestObjectResult("The header x-dotnet-6-custom-attribute can't be null or empty");
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ DotNet6CustomAttribute/Controllers/WeatherForecastController.cs
@@ -0,0 +1,49 @@
+using DotNet6CustomAttribute.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet6CustomAttribute.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("GetWeatherForecastWithCheck")]
+        [HeaderCheckAttribute]
+        public IEnumerable<WeatherForecast> GetWithCheck()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("GetWeatherForecastWithoutCheck")]
+        public IEnumerable<WeatherForecast> GetWithoutCheck()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
